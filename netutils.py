fce95e32768a023868a87d1168469509bbe220df
apostolides <thanosapostolidis@protonmail.com>
2023-01-25 19:31:04 +0200

    unpacking error fix

--- netutils.py
+++ netutils.py
@@ -5,4 +5,4 @@ def send_file(filepath, targetname, recvserv):
     with open(filepath, "rb") as f:
         contents = f.read()
         contents = base64.b64encode(contents)
-        resp = r.post(recvserv, files={targetname, contents})
+        resp = r.post(recvserv, files={targetname : contents})
